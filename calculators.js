28e9f2355e4f7df06e7b0f56e5bdc68eff57301c
Albert Hsu <iverson52000@gmail.com>
2020-03-20 10:59:10 -0700

    20200320

---
+++ calculators.js
@@ -0,0 +1,58 @@
+//Functions for claculating mean, mdeian, mode
+
+function mean(nums) {
+    var total = 0, i;
+    for (i = 0; i < nums.length; i += 1) {
+        total += nums[i];
+    }
+    return total / nums.length;
+}
+
+function median(nums) {
+    var median = 0, numsLen = nums.length;
+    nums.sort();
+ 
+    if (
+        // is even
+        numsLen % 2 === 0 
+    ) {
+        // average of two middle nums
+        median = (nums[numsLen / 2 - 1] + nums[numsLen / 2]) / 2;
+    } else { 
+        // is odd
+        // middle number only
+        median = nums[(numsLen - 1) / 2];
+    }
+ 
+    return median;
+}
+
+function mode(nums) {
+    // as result can be bimodal or multi-modal,
+    // the returned result is provided as an array
+    // mode of [3, 5, 4, 4, 1, 1, 2, 3] = [1, 3, 4]
+    var modes = [], count = [], i, number, maxIndex = 0;
+ 
+    for (i = 0; i < nums.length; i += 1) {
+        number = nums[i];
+        count[number] = (count[number] || 0) + 1;
+        if (count[number] > maxIndex) {
+            maxIndex = count[number];
+        }
+    }
+ 
+    for (i in count)
+        if (count.hasOwnProperty(i)) {
+            if (count[i] === maxIndex) {
+                modes.push(Number(i));
+            }
+        }
+ 
+    return modes;
+}
+
+module.exports = {
+  mean: mean,
+  median: median,
+  mode: mode
+};
--- server.js
+++ server.js
@@ -1,34 +1,35 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
-const knex = require('knex');
-
-const register = require('./controllers/register');
-const signin = require('./controllers/signin');
-const profile = require('./controllers/profile');
-const image = require('./controllers/image');
-
-const db = knex({
-  client: 'pg',
-  connection: {
-    connectionString : process.env.DATABASE_URL,
-    ssl: true,
-  }
-});
+const calculators = require('./calculators');
 
 const app = express();
 
 app.use(cors())
 app.use(bodyParser.json());
 
-app.get('/', (req, res)=> { res.send('it is working') })
-app.post('/signin', signin.handleSignin(db, bcrypt))
-app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
-app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
-app.put('/image', (req, res) => { image.handleImage(req, res, db)})
-app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
-
-app.listen(process.env.PORT || 3000, ()=> {
-  console.log(`app is running on port ${process.env.PORT}`);
+//test if server is working
+app.get('/', (req, res)=> { 
+  res.send('it is working') 
 })
+
+//FE will send post request
+app.post('/', (req, resp) => {
+  try{
+    let array = req.body;
+    console.log(array)
+    results = [calculators.mean(array), calculators.median(array), calculators.mode(array)];
+    resp.send(results)
+  } catch (err){
+    resp.status(400).json('error occured')
+  }
+})
+
+
+app.listen(3001, ()=> {
+  console.log(`app is running on port 3001`);
+})
+
+
+
+
