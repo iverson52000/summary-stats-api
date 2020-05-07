4a74087fad3d6e5624188b7f8923f868defdbb57
Albert Hsu <iverson52000@gmail.com>
2020-05-07 13:23:49 -0700

    prettier

--- calculators.js
+++ calculators.js
@@ -15,24 +15,24 @@ function mean(nums) {
 }
 
 function isEven(length) {
-    return (length % 2 === 0);
+    return length % 2 === 0;
 }
 
 function median(nums) {
-    if (nums.length === 0) return 0
+    if (nums.length === 0) return 0;
 
     let median = 0;
     const numsLen = nums.length;
     nums.sort();
- 
+
     if (isEven(nums.length)) {
         // average of two middle nums
         median = (nums[numsLen / 2 - 1] + nums[numsLen / 2]) / 2;
-    } else { 
+    } else {
         // is odd => middle number only
         median = nums[(numsLen - 1) / 2];
     }
- 
+
     return median;
 }
 
@@ -40,12 +40,12 @@ function mode(nums) {
     // as result can be bimodal or multi-modal,
     // the returned result is provided as an array
     // mode of [3, 5, 4, 4, 1, 1, 2, 3] = [1, 3, 4]
-    if (nums.length === 0) return 0
+    if (nums.length === 0) return 0;
 
     let modes = [];
     let counts = {};
     let maxCount = 0;
- 
+
     for (let i = 0; i < nums.length; i += 1) {
         num = nums[i];
         counts[num] = (counts[num] || 0) + 1;
@@ -53,30 +53,30 @@ function mode(nums) {
             maxCount = counts[num];
         }
     }
-    const entries = Object.entries(counts)
+    const entries = Object.entries(counts);
     for (const [num, count] of entries) {
         if (count === maxCount) {
             modes.push(Number(num));
-        };
+        }
     }
     return modes;
 }
 
 function std(nums) {
     if (nums.length === 0) return 0;
-    
+
     const avg = mean(nums);
     let residual = 0;
     for (num of nums) {
-        residual += (num-avg)**2;
+        residual += (num - avg) ** 2;
     }
-    res = (residual/nums.length)**(0.5);
+    res = (residual / nums.length) ** 0.5;
     return res;
 }
 
 module.exports = {
-  mean: mean,
-  median: median,
-  mode: mode,
-  std: std
+    mean: mean,
+    median: median,
+    mode: mode,
+    std: std,
 };
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-const express = require('express');
+const express = require("express");
 // const bodyParser = require('body-parser');
-const cors = require('cors');
-const calculators = require('./calculators');
+const cors = require("cors");
+const calculators = require("./calculators");
 
 const app = express();
 
@@ -9,32 +9,27 @@ app.use(cors());
 app.use(express.json());
 
 //test if server is working
-app.get('/', (req, res) => { 
-  res.send('it is working'); 
-})
+app.get("/", (req, res) => {
+  res.send("it is working");
+});
 
 //FE will send post request
-app.post('/', (req, resp) => {
+app.post("/", (req, resp) => {
   try {
     const array = req.body;
     console.log(array);
     results = [
-      calculators.mean(array), 
-      calculators.median(array), 
-      calculators.mode(array), 
-      calculators.std(array)
-      ];
+      calculators.mean(array),
+      calculators.median(array),
+      calculators.mode(array),
+      calculators.std(array),
+    ];
     resp.send(results);
   } catch (err) {
-    resp.status(400).json('error occured');
+    resp.status(400).json("error occured");
   }
-})
-
+});
 
 app.listen(3001, () => {
   console.log(`app is running on port 3001`);
-})
-
-
-
-
+});
