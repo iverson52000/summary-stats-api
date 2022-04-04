8b8cf62eb0d33df17f97ed64c6245c26b8777ba6
alberthsuu <alberthsuu@google.com>
2022-04-04 17:05:55 +0000

    Correct type

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const calculators = require(s"./calculators");
+const calculators = require("./calculators");
 
 const app = express();
 
